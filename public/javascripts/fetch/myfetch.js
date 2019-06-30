6f9007f95650365a27296ae95431d00ec789dcd1
Sambit <sambit.sarkar@hyland.com>
2019-06-30 22:54:53 +0530

    Made container scollable.

--- public/javascripts/fetch/myfetch.js
+++ public/javascripts/fetch/myfetch.js
@@ -1,21 +1,20 @@
-let item1OnClick = () => {
-    alert(1);
+let fetchWithFullRemoteUrl = () => {
+    let newurl = '/fetch';
+    alert(newurl);
+    fetch(newurl, {
+        //credentials: 'include',
+        // method: 'put',
+        // mode: 'no-cors'
+    }).then(function (response) {
+        //alert(response);
+        return response.text();
+    }).then(function (data) {
+        alert(data);
+    });
 }
 
-let item2OnClick = () => {
-    alert(2);
-}
-
-/*
-
-setTimeout(() => {
-    let btn = document.createElement('button');
-    
-    btn.innerHTML = 'fetch with full remote url';
-    //var url = document.getElementById("urltextbox").value;
-    
-    btn.onclick = function (evt) {
-        var url = document.getElementById("urltextbox").value;
+let fetchWithFullRemoteUrl2 = () => {
+    var url = document.getElementById("urltextbox").value;
         //alert(url);
         let newurl= url + '/fetch';
         alert(newurl);
@@ -30,87 +29,50 @@ setTimeout(() => {
         }).then(function (data) {
             alert(data);
         });
-    }
-    //document.getElementById('main-page').appendChild("br");
-    document.getElementById('main-page').appendChild(btn);
-    //document.getElementById('main-page').appendChild("br");
-    
-}, 1000);
+}
 
-setTimeout(() => {
-    let btn = document.createElement('button');
-    btn.innerHTML = 'fetch with full remote url cors https';
-    btn.onclick = function (evt) {
-        fetch('https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies', {
+let fetchWithFullRemoteUrlWithCorsHttps = () => {
+    fetch('https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies', {
             //credentials: 'include',
             // method: 'put',
             // mode: 'no-cors'
         }).then(function (response) {
             alert(response);
         });
-    }
-    //document.getElementById('main-page').appendChild("br");
-    document.getElementById('main-page').appendChild(btn);
-    
-}, 1000);
+}
 
-setTimeout(() => {
-    let btn = document.createElement('button');
-    btn.innerHTML = 'fetch with relative remote url-no-cors';
-    btn.onclick = function (evt) {
-        fetch('//google.com', {mode: 'no-cors'},{
+let fetchWithRelativeRemoteUrlNoCors = () => {
+    fetch('//google.com', {mode: 'no-cors'},{
             //credentials: 'include',
         }).then(function (response) {
             return response.text();
         }).then(function (data){
             alert(data);
         });
-    }
-    //document.getElementById('main-page').appendChild("br");
-    document.getElementById('main-page').appendChild(btn);
-    //document.getElementById('main-page').appendChild(createElement("br"));
-}, 1000);
+}
 
-setTimeout(() => {
-    let btn = document.createElement('button');
-    btn.innerHTML = 'fetch with relative remote url-cors';
-    btn.onclick = function (evt) {
-        fetch('//google.com', {mode: 'cors'},{
+let fetchWithRelativeRemoteUrlCors = () => {
+    fetch('//google.com', {mode: 'cors'},{
             //credentials: 'include',
         }).then(function (response) {
             return response.text();
         }).then(function (data){
             alert(data);
         });
-    }
-    //document.getElementById('main-page').appendChild("br");
-    document.getElementById('main-page').appendChild(btn);
-   // document.getElementById('main-page').appendChild(createElement("br"));
-}, 1000);
+}
 
-setTimeout(() => {
-    let btn = document.createElement('button');
-    btn.innerHTML = 'fetch with relative remote url-same-origin';
-    btn.onclick = function (evt) {
-        fetch('../../../relative/a.txt', {mode: 'same-origin'},{
-            //credentials: 'include',
-        }).then(function (response) {
-            return response.text();
-        }).then(function (data){
-            alert(data);
-        });
-    }
-   // document.getElementById('main-page').appendChild("br");
-    document.getElementById('main-page').appendChild(btn);
-    //document.getElementById('main-page').appendChild(createElement("br"));
-}, 1000);
+let fetchWithRelativeUrlSameOrigin = () => {
+    fetch('../../../relative/a.txt', {mode: 'same-origin'},{
+        //credentials: 'include',
+    }).then(function (response) {
+        return response.text();
+    }).then(function (data){
+        alert(data);
+    });
+}
 
-setTimeout(() => {
-    let btn = document.createElement('button');
-    btn.innerHTML = 'fetch and post';
-    btn.onclick = function (evt) {
-        var url = document.getElementById("urltextbox").value;
-        let newurl= url + '/fetch/post';
+let fetchWithPost = () => {
+        let newurl= '/fetch/post';
         alert(newurl);
         fetch(newurl, {
             method: 'POST',
@@ -122,44 +84,10 @@ setTimeout(() => {
         }).then(function (data){
             alert(data);
         });
-    }
-    //document.getElementById('main-page').appendChild("br");
-    document.getElementById('main-page').appendChild(btn);
-   // document.getElementById('main-page').appendChild(createElement("br"));
-}, 1000);
+}
 
-setTimeout(() => {
-    let btn = document.createElement('button');
-    btn.innerHTML = 'fetch and put';
-    btn.onclick = function (evt) {
-        var url = document.getElementById("urltextbox").value;
-        //alert(url);
-        let newurl= url + '/fetch/put';
-        alert(newurl);
-        fetch(newurl, {
-            method: 'PUT',
-            body: 'HELLO',
-            headers: new Headers({'Content-Type': 'text/plain',
-            'Accept': 'text/plain'
-        })
-            //credentials: 'include',
-        }).then(function (response) {
-            return response.text();
-        }).then(function (data){
-            alert(data);
-        });
-    }
-    //document.getElementById('main-page').appendChild("br");
-    document.getElementById('main-page').appendChild(btn);
-    //document.getElementById('main-page').appendChild(createElement("br"));
-}, 1000);
-
-setTimeout(() => {
-    let btn = document.createElement('button');
-    btn.innerHTML = 'fetch and delete';
-    btn.onclick = function (evt) {
-
-        var url = document.getElementById("urltextbox").value;
+let fetchAndDelete = () => {
+    var url = document.getElementById("urltextbox").value;
         //alert(url);
         let newurl= url + '/fetch/delete';
         alert(newurl);
@@ -175,23 +103,31 @@ setTimeout(() => {
         }).then(function (data){
             alert(data);
         });
-    }
-   // document.getElementById('main-page').appendChild("br");
-    document.getElementById('main-page').appendChild(btn);
-   // document.getElementById('main-page').appendChild(createElement("br"));
-}, 1000);
+}
 
-setTimeout(() => {
-    let btn = document.createElement('button');
-    btn.innerHTML = 'fetch and head';
-    btn.onclick = function (evt) {
-        var url = document.getElementById("urltextbox").value;
-        //alert(url);
-        let newurl= url + '/fetch/head';
+let fetchAndPut = () => {
+        let newurl= '/fetch/put';
+        alert(newurl);
+        fetch(newurl, {
+            method: 'PUT',
+            body: 'HELLO',
+            headers: new Headers({'Content-Type': 'text/plain',
+            'Accept': 'text/plain'
+        })
+            //credentials: 'include',
+        }).then(function (response) {
+            return response.text();
+        }).then(function (data){
+            alert(data);
+        });
+}
+
+let fetchAndHead = () => {
+        let newurl= '/fetch/head';
         alert(newurl);
         fetch(newurl, {
             method: 'HEAD',
-            
+
             headers: new Headers({'Content-Type': 'text/plain',
             'Accept': 'text/plain'
         })
@@ -201,24 +137,15 @@ setTimeout(() => {
         }).then(function (data){
             alert(data);
         });
-    }
-    //document.getElementById('main-page').appendChild("br");
-    document.getElementById('main-page').appendChild(btn);
-   // document.getElementById('main-page').appendChild(createElement("br"));
-}, 1000);
+}
 
-setTimeout(() => {
-    let btn = document.createElement('button');
-    btn.innerHTML = 'fetch and patch';
-    btn.onclick = function (evt) {
-
-        var url = document.getElementById("urltextbox").value;
+let fetchAndPatch = () => {
         //alert(url);
-        let newurl= url + '/fetch/patch';
+        let newurl=  '/fetch/patch';
         alert(newurl);
         fetch(newurl, {
             method: 'PATCH',
-            
+
             headers: new Headers({'Content-Type': 'text/plain',
             'Accept': 'text/plain'
         })
@@ -228,41 +155,12 @@ setTimeout(() => {
         }).then(function (data){
             alert(data);
         });
-    }
-   // document.getElementById('main-page').appendChild("br");
-    document.getElementById('main-page').appendChild(btn);
-    //document.getElementById('main-page').appendChild(createElement("br"));
-}, 1000);
+}
 
-
-
-setTimeout(() => {
-    let btn = document.createElement('button');
-    btn.innerHTML = 'fetch with relative url';
-    btn.onclick = function (evt) {
-        fetch('ok', {
-            //credentials: 'include',
-        }).then(function (response) {
-            alert(response);
-        });
-    }
-    document.getElementById('main-page').appendChild(btn);
-}, 1000);
-
-setTimeout(() => {
-    let btn = document.createElement('button');
-    btn.innerHTML = 'fetch with full local url for loading file';
-    btn.onclick = function (evt) {
-        fetch('file:///Users/ssarkar/Library/Developer/CoreSimulator/Devices/4A942F76-991E-4A50-89D0-C536A3687192/data/Containers/Bundle/Application/B6847970-90D5-47F9-B7E1-5DB5DBEED924/Minotaur.app/Web/fs.js', {
-            //credentials: 'include',
-        }).then(function (response) {
-            alert(response);
-        });
-    }
-    //document.getElementById('main-page').appendChild("br");
-    //document.getElementById('main-page').appendChild(document.createComment("br"));
-    document.getElementById('main-page').appendChild(btn);
-    
-}, 1000);
-
-*/
+let fetchWithRelativeUrl = () => {
+    fetch('ok', {
+        //credentials: 'include',
+    }).then(function (response) {
+        alert(response);
+    });
+}
